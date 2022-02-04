ac964277a48ec7487e14fad1641d12c058a4d8c2
Najmul Huda <najmul.myself@gmail.com>
2022-02-04 20:14:26 +0600

    need to structured the map

---
+++ lib/controller/kit.dart
@@ -0,0 +1,11 @@
+class Kit {
+  String? name;
+  List? link;
+  int? id;
+
+  Kit({this.name, this.id, this.link});
+}
+
+
+
+Kit barca = Kit(id: 1,name: 'Barca',link: [{'link':'imgwwwcom','img':'dummy.img'}]);
--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dls_kit/widgets/newpage.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,10 +12,20 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage("https://picsum.photos/200"),
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => NewPage(),
+                    ),
+                  );
+                },
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage("https://picsum.photos/200"),
+                ),
               ),
               CircleAvatar(
                 radius: 50,
