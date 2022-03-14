646f5c0dfaf4b1a66509d3e32432eea4b6a3717f
najmulmyself <najmul.myself@gmail.com>
2022-03-14 21:56:01 +0600

    national team added

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -9,28 +9,45 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Image.asset('assets/images/Dream.png'),
-                ],
-              )
-              // CircleAvatar(
-              //   radius: 50,
-              //   backgroundImage: NetworkImage("https://picsum.photos/200"),
-              // ),
-              // CircleAvatar(
-              //   radius: 50,
-              //   backgroundImage: NetworkImage("https://picsum.photos/200"),
-              // ),
-              // CircleAvatar(
-              //   radius: 50,
-              //   backgroundImage: NetworkImage("https://picsum.photos/200"),
-              // ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Image.asset('assets/images/Dream.png'),
+                    // SizedBox(
+                    //   height: 20,
+                    // ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('assets/images/brazil.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('assets/images/brazil.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('assets/images/brazil.png'),
+                    ),
+                  ],
+                )
+                // CircleAvatar(
+                //   radius: 50,
+                //   backgroundImage: NetworkImage("https://picsum.photos/200"),
+                // ),
+                // CircleAvatar(
+                //   radius: 50,
+                //   backgroundImage: NetworkImage("https://picsum.photos/200"),
+                // ),
+                // CircleAvatar(
+                //   radius: 50,
+                //   backgroundImage: NetworkImage("https://picsum.photos/200"),
+                // ),
+              ],
+            ),
           ),
         ),
       ),
